19bbda9524efe727e316b4742b77f2689c3a7399
slabcouski <prof.marcelo.garcia@docente.pr.senac.br>
2023-05-18 22:27:21 -0300

    18/05/23 22:27

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,16 @@ $(document).ready(function() {
 
         let newList = $('#new-task-list');
         let newItem = $(`<li></li>`);
-        let newCheck = $('<input type="checkbox" id="check">');
+        let newCheck = $('<input type="checkbox">');
         $(newCheck).appendTo(newItem);
         $(`<p id="task"></p>`).text(newList.val()).appendTo(newItem);
         $(newItem).appendTo('#new-task');
         newItem.after('<hr>');
         $(newList).val('');
         
-        $('#check').click(function() {
-            $('#task').toggleClass("done");
-        });
+    });
+    $('ul').on('click', 'li', function() {
+        $(this).toggleClass('done');
     });
 });
+
